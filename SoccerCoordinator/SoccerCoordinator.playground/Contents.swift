475cbd9b3f2cd337867cba3d985a33485550e5c4
James Lee <jameslee@JamesdeMac.local>
2017-04-17 21:58:52 +0800

    enter players's data

--- SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
@@ -2,6 +2,26 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var player01 = ["name": "Joe Smith", "height": 42, "experienced": true, "guardian": "Jim and Jan Smith"] as [String : Any]
+var player02 = ["name": "Jill Tanner", "height": 36, "experienced": true, "guardian": "Ciara Tanner"] as [String : Any]
+var player03 = ["name": "Bill Bon", "height": 43, "experienced": true, "guardian": "Sara and Jenny Bon"] as [String : Any]
+var player04 = ["name": "Eva Gordon", "height": 45, "experienced": false, "guardian": "Wendy and Mike Gordon"] as [String : Any]
+var player05 = ["name": "Matt Gill", "height": 40, "experienced": false, "guardian": "Charles and Sylvia Gill"] as [String : Any]
+var player06 = ["name": "Kimmy Stein", "height": 41, "experienced": false, "guardian": "Bill and Hillary Stein"] as [String : Any]
+var player07 = ["name": "Sammy Adams", "height": 45, "experienced": false, "guardian": "Jeff Adams"] as [String : Any]
+var player08 = ["name": "Karl Saygan", "height": 42, "experienced": true, "guardian": "Heather Biedsoe"] as [String : Any]
+var player09 = ["name": "Suzane Greenberg", "height": 44, "experienced": true, "guardian": "Henrietta Dumas"] as [String : Any]
+var player10 = ["name": "Sal Dall", "height": 41, "experienced": false, "guardian": "Gala Dail"] as [String : Any]
+var player11 = ["name": "Joe Kavalier", "height": 39, "experienced": false, "guardian": "Sam and Elaine Kavaller"] as [String : Any]
+var player12 = ["name": "Ben Finkeistein", "height": 44, "experienced": false, "guardian": "Aaron and Jill Finkeistein"] as [String : Any]
+var player13 = ["name": "Diego Soto", "height": 41, "experienced": true, "guardian": "Robin and Sarika Soto"] as [String : Any]
+var player14 = ["name": "Chioe Alaska", "height": 47, "experienced": false, "guardian": "David and Jamie Alaska"] as [String : Any]
+var player15 = ["name": "Arnoid Willis", "height": 43, "experienced": false, "guardian": "Claire Willis"] as [String : Any]
+var player16 = ["name": "Phillip Helm", "height": 44, "experienced": true, "guardian": "Thomas Helm and Eva Jones"] as [String : Any]
+var player17 = ["name": "Les Clay", "height": 42, "experienced": true, "guardian": "Wynonna Brown"] as [String : Any]
+var player18 = ["name": "Herschel Krustofski", "height": 45, "experienced": true, "guardian": "Hyman and Rachel Krustofski"] as [String : Any]
 
-//0417 comment
+
+
+
+
